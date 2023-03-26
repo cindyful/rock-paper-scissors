49bcefa35f56316603c21843cfb9423f51028dbe
Cindy <nguyen.cindy17@gmail.com>
2023-03-26 15:51:20 -0400

    Make function for random computer selection

--- script.js
+++ script.js
@@ -1,10 +1,44 @@
-// let playerSelection = prompt("Please choose rock, paper, or scissors.");
+let choices = ["rock", "paper", "scissors"];
 
-let choices = ["Rock","Paper","Scissors"];
-let getComputerChoice = choices[Math.floor(Math.random()*choices.length)];
+function getComputerChoice(choices) {
+    return choices[Math.floor(Math.random()*choices.length)];
+};
 
+let computerSelection = getComputerChoice(choices);
+
+
+
+
+
+/*
 const choiceButton = document.querySelector('.choices');
 
-choiceButton.addEventListener("click", () => {
-    console.log("button clicked");
-});
+//let playerSelection = document.querySelector(".choices").value;
+let playerSelection = "rock";
+const computerSelection = getComputerChoice();
+
+choiceButton.addEventListener("click", playRound);
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "It's a tie!";
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        return "You win! Rock beats scissors.";
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        return "You lose! Paper beats rock.";
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        return "You win! Paper beats rock.";
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        return "You lose! Scissors beats paper.";
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return "You win! Scissors beats paper.";
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        return "You lose! Rock beats scissors.";
+    }
+}
+
+playRound();
+
+*/
+    
+
